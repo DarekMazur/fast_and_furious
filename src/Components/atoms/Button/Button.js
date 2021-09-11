9492468852a946ea1b8f07f3a1c48cc331145945
Darek Mazur <dev@darekmazur.it>
2021-09-12 01:33:14 +0200

    add edit and add new icons

--- src/Components/atoms/Button/Button.js
+++ src/Components/atoms/Button/Button.js
@@ -1,5 +1,19 @@
 import bin from '../../../assets/svg/bin.svg';
+import edit from '../../../assets/svg/edit.svg';
+import add from '../../../assets/svg/add.svg';
 
-const Button = (props) => <button {...props}>{props.buttonType === 'del' ? <img src={bin} alt="Trash can" /> : props.buttonType}</button>;
+const Button = (props) => (
+  <button {...props}>
+    {props.buttonType === 'del' || props.buttonType === 'edit' ? (
+      props.buttonType === 'del' ? (
+        <img src={bin} alt="Trash can" />
+      ) : (
+        <img src={edit} alt="Edit pad" />
+      )
+    ) : (
+      <img src={add} alt="Add new item" />
+    )}
+  </button>
+);
 
 export default Button;
